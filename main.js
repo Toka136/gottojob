ace6dacaefeb18fa4c8f0294c967cf582fb63934
Toka136 <145512601+Toka136@users.noreply.github.com>
2024-09-03 18:51:42 +0300

    test344
    test44

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 window.onscroll = function(){
     let elem = document.getElementById('header');
     if( window.scrollY > 250 ) {
-        elem.style.cssText = "position:fixed;width:100%;top:0;background: var(--bs-black);color: white; opacity: .7;x-index:9;transition: 1s;";
+        elem.style.cssText = "position:fixed;width:100%;top:0;background: var(--bs-black);color: white; opacity: .7;z-index:9;transition: 1s;";
         let links=document.getElementsByClassName("head-white");
         for(var i=0;i<links.length;i++)
         {
